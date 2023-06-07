4d0004454cb9667653aea9cadb2fb80f4271ff66
Naveen JM <jmnaveen90@gmail.com>
2023-06-07 16:23:58 +0530

    to bar and icons added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { colorModeContext, themeSettings, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+
 function App() {
-  return <div className="App"></div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <colorModeContext.Provider value="colorMode">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+        ;
+      </ThemeProvider>
+    </colorModeContext.Provider>
+  );
 }
 
 export default App;
